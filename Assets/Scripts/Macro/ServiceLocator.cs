932d9d17eaa703b445623ebd733c0cf1841a31e6
AreniumIon <jeffreydickenson9@gmail.com>
2021-11-18 15:16:14 -0600

    ServiceLocater created

---
+++ Assets/Scripts/Macro/ServiceLocator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class ServiceLocator
+{
+    private static readonly Dictionary<Type, object> Services = new Dictionary<Type, object>();
+
+    public static void Register<T>(object serviceInstance)
+    {
+        Services[typeof(T)] = serviceInstance;
+    }
+
+    public static T Resolve<T>()
+    {
+        return (T)Services[typeof(T)];
+    }
+
+    public static void Reset()
+    {
+        Services.Clear();
+    }
+}
