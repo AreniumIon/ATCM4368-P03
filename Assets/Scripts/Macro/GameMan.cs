75199fb08a197979b3ec9cf39f6ff4f4ad1a6698
AreniumIon <jeffreydickenson9@gmail.com>
2021-11-19 19:37:14 -0600

    Health Bar adjustment

--- Assets/Scripts/Macro/GameMan.cs
+++ Assets/Scripts/Macro/GameMan.cs
@@ -25,17 +25,4 @@ public class GameMan : MonoBehaviour
             ServiceLocator.Register<GameMan>(this);
         }
     }
-
-    private void Start()
-    {
-        //SetGameParams(PlayerConstructor.GetPlayerInfo(PlayerID.Normal));
-    }
-
-    public void SetGameParams(PlayerInfo playerInfo)
-    {
-        GameObject playerManObject = PlayerConstructor.CreatePlayer(playerInfo.playerID, ServiceLocator.GetService<GameUIMan>().PlayerUI.canvasObj.transform);
-        PlayerMan = playerManObject.GetComponent<PlayerMan>();
-
-        PlayerMan.SetParams((EntityInfo)playerInfo);
-    }
 }
