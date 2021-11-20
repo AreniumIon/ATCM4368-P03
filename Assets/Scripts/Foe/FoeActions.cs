437566e3752b25771927c3eab36d203154a7130d
AreniumIon <jeffreydickenson9@gmail.com>
2021-11-20 01:56:55 -0600

    FoeActions implemented

--- Assets/Scripts/Foe/FoeActions.cs
+++ Assets/Scripts/Foe/FoeActions.cs
@@ -1,14 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameConstants;
 
 public class FoeActions : MonoBehaviour
 {
     FoeMan fm;
 
+    List<ActionInfo> actions = new List<ActionInfo>();
 
     public void SetParams(FoeMan fm)
     {
         this.fm = fm;
+
+        actions = fm.foeInfo.actions;
+    }
+
+    public ICommand GetRandomCommand()
+    {
+        ActionInfo actionInfo = GetRandomAction();
+        ICommand command = CommandConstructor.CreateCommand(actionInfo.commandID, actionInfo.value, GetTarget(actionInfo.commandID));
+
+        return command;
+    }
+
+    private ActionInfo GetRandomAction()
+    {
+        int choice = Random.Range(0, actions.Count);
+        ActionInfo action = actions[choice];
+
+        return action;
+    }
+
+    private Attackable GetTarget(CommandID commandID)
+    {
+        GameMan gameMan = ServiceLocator.GetService<GameMan>();
+
+        switch (commandID)
+        {
+            default:
+            case CommandID.Attack:
+                return gameMan.PlayerMan.PlayerHealth;
+            case CommandID.Defend:
+                return gameMan.FoeMan.FoeHealth;
+            case CommandID.Heal:
+                return gameMan.FoeMan.FoeHealth;
+        }
     }
 }
